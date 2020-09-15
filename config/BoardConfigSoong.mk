1c4e265a595d6d2ff450052adc7bfc59d6b6b187
sbwml <984419930@qq.com>
2020-09-15 14:14:15 +0800

    update BoardConfigSoong.mk

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -5,6 +5,8 @@ ORIG_PATH := $(shell cat $(OUT_DIR)/.path_interposer_origpath)
 endif
 EXPORT_TO_SOONG := \
     KERNEL_ARCH \
+    KERNEL_CC \
+    KERNEL_CLANG_TRIPLE \
     KERNEL_BUILD_OUT_PREFIX \
     KERNEL_CROSS_COMPILE \
     KERNEL_MAKE_FLAGS \
