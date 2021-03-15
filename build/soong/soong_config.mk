202e9cbd85e14fb90cbb82c4d1c95b1ca82dbeed
ittat <ittat@live.com>
2021-03-15 08:49:30 +0800

    Update soong_config.mk

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -1,6 +1,6 @@
 FROM_SOONG := true
 
-include vendor/aosp/config/BoardConfigQcom.mk
+include vendor/b2g/config/BoardConfigQcom.mk
 
 add_json_str_omitempty = $(if $(strip $(2)),$(call add_json_str, $(1), $(2)))
 add_json_val_default = $(call add_json_val, $(1), $(if $(strip $(2)), $(2), $(3)))
