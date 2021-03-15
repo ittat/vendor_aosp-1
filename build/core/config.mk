140c8442094a8458938cb9500cf8901e81d7af83
ittat <ittat@live.com>
2021-03-15 08:48:47 +0800

    Update config.mk

--- build/core/config.mk
+++ build/core/config.mk
@@ -1,4 +1,4 @@
-BUILD_RRO_SYSTEM_PACKAGE := $(TOPDIR)vendor/b2g/build/core/system_rro.mk
+#BUILD_RRO_SYSTEM_PACKAGE := $(TOPDIR)vendor/b2g/build/core/system_rro.mk
 
 # Rules for QCOM targets
 include $(TOPDIR)vendor/b2g/build/core/qcom_target.mk
